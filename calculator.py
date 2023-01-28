399a55db65d65d940491f9dfb2a33912f7a6e147
RbDev <randell@live.nl>
2023-01-28 21:50:49 +0100

    input settings

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# Settings
+resin_type = "Resin type x"
+price_resin = 5
+hardner_type = "Hardner type x"
+price_hardner = 2
+resin_ratio = 2
+hardner_ratio = 1
+carbon_type = "3k"
+default_carbon_size = 1 # per square meter
+price_carbon = 20 # per default_carbon_size
