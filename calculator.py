7c2b25b92832be7d1d386503d0af5a33f84c0df7
RbDev <randell@live.nl>
2023-01-29 23:28:06 +0100

    adding calculation function

--- calculator.py
+++ calculator.py
@@ -16,25 +16,36 @@ default_carbon_area = default_carbon_size_width * default_carbon_size_height #m2
 default_carbon_weight = 200 #grams
 price_carbon = 43
 
+# User input section
 name_calculation = input("Name this session: ")
 required_area = float(input("What is the needed area in m2: "))
 amount_layers = int(input("How many layers is needed: "))
 
 print("########################################")
 
-print("Name of session: " + str(name_calculation))
-print("Area of part: " + str(required_area))
-print("Amount of layers: " + str(amount_layers))
+# Calculation
+def calc_amount(required_area, amount_layers):
+    calc_req_area = (required_area / default_carbon_area) * amount_layers
+    calc_req_resin = (calc_req_area * default_carbon_weight) / 100 * 120
+    calc_req_hardner = (calc_req_resin / 2) / 100 * 120
+    est_cost = ((calc_req_resin / resin_weight) * price_resin) + ((calc_req_hardner / hardner_weight) * price_resin) + calc_req_area * price_carbon
 
-print("########################################")
+    result = {
+        "area" : str(round(calc_req_area, 3)),
+        "resin" : str(round(calc_req_resin, 3)),
+        "hardner" : str(round(calc_req_hardner, 3)),
+        "cost" : str(round(est_cost, 3))
+    }
 
-calc_req_area = (required_area / default_carbon_area) * amount_layers
-calc_req_resin = (calc_req_area * default_carbon_weight) / 100 * 120
-calc_req_hardner = (calc_req_resin / 2) / 100 * 120
+    return result
 
-print("The amount of carbon: " + str(calc_req_area) + " m2")
-print("The amount of resin: " + str(calc_req_resin) + " grams")
-print("The amount of hardner: " + str(calc_req_hardner) + " grams")
+
+calculation = calc_amount(required_area, amount_layers)
+
+# Result print
+print("The amount of carbon: " + calculation["area"] + " m2")
+print("The amount of resin: " + calculation["resin"] + " grams")
+print("The amount of hardner: " + calculation["hardner"] + " grams")
+print("Estimated cost: " + calculation["cost"])
 
 print("End of program")
-
